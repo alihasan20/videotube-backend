df213a00ce56596c15f7ab6012f29e2af79bc7c1
sohail khan <84376032+Sohail700@users.noreply.github.com>
2025-08-20 01:19:28 +0530

    Update cloudinary.js

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,49 +1,25 @@
-import { v2 as cloudinary} from "cloudinary";
-import fs from "fs"
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
-// Configuration
-    cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-        api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDINARY_API_SECRET// Click 'View API Keys' above to copy your API secret
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // <-- Fixed
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
     });
-    
-// Upload an image
-     const uploadonCloudinary = async (localFilePath) => {
-      try {
-        if (!localFilePath) return null
-        // upload the file on cloudinary
-        const response = await cloudinary.uploader.upload(
-           localFilePath, {
-               resource_type: "auto"
-           }
-       )
-       // file has been uploaded successfully
-       console.log("file is uploaded on cloudinary", response.url);
-       return response
-       
-      } catch(error){
-           fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
-           return null
-      }
-    }
+    // console.log("file is uploaded on cloundinary", response.url);
+    fs.unlinkSync(localFilePath);
+    console.log(response);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove temp file from local storage
+    console.log(error);
+  }
+};
 
-    export {uploadonCloudinary}
-    
-// // Optimize delivery by resizing and applying auto-format and auto-quality
-//     const optimizeUrl = cloudinary.url('shoes', {
-//         fetch_format: 'auto',
-//         quality: 'auto'
-//     });
-    
-//     console.log(optimizeUrl);
-    
-// // Transform the image: auto-crop to square aspect_ratio
-//     const autoCropUrl = cloudinary.url('shoes', {
-//         crop: 'auto',
-//         gravity: 'auto',
-//         width: 500,
-//         height: 500,
-//     });
-    
-//     console.log(autoCropUrl);
+export { uploadOnCloudinary };
